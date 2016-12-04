766101638c3ebb6dad0be59ab49be0f770904e0e
Gabriel Mahan <mahan.gabe@gmail.com>
2016-12-04 16:40:09 -0500

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import head from './head.png'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      display: 'NAV'
+    }
+    this.changeNav = (m) => {this.setState({display: m})};
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {this.state.display === 'NAV' ?
+            <div className="nav">
+              <span>What brings you here?</span>
+              <div className="option-container">
+                <div > <span>You're a parent or student </span></div>
+                <div className="hire" onClick={() => {this.changeNav("HIRE")}}> <span id="balancer" >Hire Gabe</span></div>
+                <div > <span>Fountain Pens</span></div>
+                <div > <span>Pictures of Bernese Mountain Dogs</span></div>
+                <div > <span> Whiskey Reccomendation </span></div>
+                <div > <span> Hi, Mom  </span></div>
+
+                <div onClick={() => {this.changeNav("ALLY")}}> <span> Altara </span></div>
+              </div>
+            </div>
+          : null
+          }
+          {this.state.display === 'ALLY' ?
+            <div className="ally">
+              hey!
+            </div>
+          : null
+          }
+          {this.state.display === 'HIRE' ?
+            <div className="ally">
+              hey!
+            </div>
+          : null
+          }
+
+          <img src={head} />
+          <div className="nametag">
+            <div>
+              <h1>Gabriel Mahan</h1>
+              <span>Web Developer</span>
+              <span>Educator</span>
+            </div>
+          </div>
+
+
       </div>
     );
   }
